41322bc6668f9a5c11e7226f481c12f8f350f5a4
littleblack111 <littleblack11111@gmail.com>
2026-01-01 12:54:40 +0800

    feat: added support for `Option`

--- src/lib.rs
+++ src/lib.rs
@@ -86,3 +86,76 @@ impl<T, E: Error, Fut: Future<Output = Result<T, E>>, F: FnMut() -> Fut> Retryab
         }
     }
 }
+
+pub trait RetryableOptionFn<T> {
+    fn unwrap_blocking(self) -> T;
+}
+
+impl<T, F: FnMut() -> Option<T>> RetryableOptionFn<T> for F {
+    #[cfg_attr(
+        feature = "track-caller",
+        track_caller
+    )]
+    fn unwrap_blocking(mut self) -> T {
+        let caller = Location::caller();
+        let mut printed = false;
+
+        loop {
+            match self() {
+                Some(v) => return v,
+                None => {
+                    if !printed {
+                        if cfg!(feature = "track-caller") {
+                            println!(
+                                "None at {}:{}:{}, will block till Some...",
+                                caller.file(),
+                                caller.line(),
+                                caller.column()
+                            );
+                        } else {
+                            println!("None, will block till Some...");
+                        }
+                        printed = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub trait RetryableOptionAsyncFn<T> {
+    async fn unwrap_opt(self, wait: Option<Duration>) -> T;
+}
+
+impl<T, Fut: Future<Output = Option<T>>, F: FnMut() -> Fut> RetryableOptionAsyncFn<T> for F {
+    #[cfg_attr(
+        feature = "track-caller",
+        track_caller
+    )]
+    async fn unwrap_opt(mut self, wait: Option<Duration>) -> T {
+        let caller = Location::caller();
+        let mut printed = false;
+
+        loop {
+            match self().await {
+                Some(v) => return v,
+                None => {
+                    if !printed {
+                        if cfg!(feature = "track-caller") {
+                            println!(
+                                "None at {}:{}:{}, will block till Some...",
+                                caller.file(),
+                                caller.line(),
+                                caller.column()
+                            );
+                        } else {
+                            println!("None, will block till Some...");
+                        }
+                        printed = true;
+                    }
+                }
+            }
+            sleep(wait.unwrap_or(INTERVAL_MS));
+        }
+    }
+}
